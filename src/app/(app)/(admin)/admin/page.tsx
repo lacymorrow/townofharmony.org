e99f5a5c86622e366a60107ac39c3759ed635974
Lacy Morrow <me@lacymorrow.com>
2026-05-06 11:14:06 -0400

    fix(admin): redirect /admin to Builder.io CMS (LAC-621)

--- src/app/(app)/(admin)/admin/page.tsx
+++ src/app/(app)/(admin)/admin/page.tsx
@@ -1,9 +1,5 @@
 import { redirect } from "next/navigation";
-import { routes } from "@/config/routes";
 
-/**
- * Admin page
- */
 export default async function AdminPage() {
-  redirect(routes.admin.users);
+  redirect("https://builder.io/content");
 }
